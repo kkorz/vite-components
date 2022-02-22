13088d75fa74007a6b77ecddc2d524c3f18de1d1
zhaokang <1291023564@qq.com>
2022-02-22 18:17:43 +0800

    feat: add milkdown editor

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,5 +1,21 @@
+import { defaultValueCtx, Editor, rootCtx } from "@milkdown/core";
+import { nord } from "@milkdown/theme-nord";
+import { ReactEditor, useEditor } from "@milkdown/react";
+import { commonmark } from "@milkdown/preset-commonmark";
+import { emoji } from "@milkdown/plugin-emoji";
+
 function Welcome() {
-  return <div>Welcome</div>;
+  const editor = useEditor((root) =>
+    Editor.make()
+      .config((ctx) => {
+        ctx.set(rootCtx, root);
+        ctx.set(defaultValueCtx, "# Milkdown Editor :heartpulse: Vite React");
+      })
+      .use(nord)
+      .use(commonmark)
+      .use(emoji)
+  );
+  return <ReactEditor editor={editor} />;
 }
 
 export default Welcome;
